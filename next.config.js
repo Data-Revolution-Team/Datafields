ab1e2aa6ff7636dfc104cd12a48cf64334ed3ce0
Jacob Somerville <j.somerville97@gmail.com>
2025-08-31 15:53:12 +0930

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     reactStrictMode: true,
     basePath: isProd ? '/Datafields' : '',
     output: 'export',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 module.exports = nextConfig
