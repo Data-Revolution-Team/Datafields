4ddfdb212df748a296cea592b8c6b62b1c229cb9
Jacob Somerville <j.somerville97@gmail.com>
2025-08-31 15:49:29 +0930

    fix: next config

--- next.config.js
+++ next.config.js
@@ -2,19 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     reactStrictMode: true,
-    trailingSlash: true,
-    assetPrefix: isProd ? '/Datafields/' : '',
     basePath: isProd ? '/Datafields' : '',
     output: 'export',
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: {
-        unoptimized: true,
-    },
 };
 
 module.exports = nextConfig
